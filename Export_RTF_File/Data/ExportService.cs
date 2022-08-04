f429c777a8549370a3556bbaf9d0ceeeb35f6279
Saravanan G <saravanang@syncfusion.com>
2022-08-04 13:01:55 +0530

    Add files via upload

---
+++ Export_RTF_File/Data/ExportService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Syncfusion.Pdf;
+using Syncfusion.HtmlConverter;
+
+namespace Export_RTF_File.Data
+{
+    public class ExportService
+    {
+        public MemoryStream ExportAsPdf(string content)
+        {
+            try
+            {
+                // Initialize the HTML to PDF converter
+                HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
+
+                WebKitConverterSettings settings = new WebKitConverterSettings();
+
+                // Used to load resources before convert
+                // Map your local path here
+                string baseUrl = @"C:/Users/TempKaruna/source/repos/RTEtoPDF/wwwroot/images";
+
+                // Set WebKit path
+                // Map your local path installed location here
+                settings.WebKitPath = @"C:/Program Files (x86)/Syncfusion/HTMLConverter/18.1.0.52/QtBinariesDotNetCore";
+
+                // Set additional delay; units in milliseconds;
+                settings.AdditionalDelay = 3000;
+
+                // Assign WebKit settings to HTML converter
+                htmlConverter.ConverterSettings = settings;
+
+                // Convert HTML string to PDF
+                PdfDocument document = htmlConverter.Convert(content, baseUrl);
+
+                // Save the document into stream.
+                MemoryStream stream = new MemoryStream();
+
+                document.Save(stream);
+
+                stream.Position = 0;
+
+                // Close the document.
+                document.Close(true);
+
+                return stream;
+            }
+            catch
+            {
+                return new MemoryStream();
+            }
+        }
+    }
+}
---
+++ Export_RTF_File/SampleInterop.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Newtonsoft.Json;
+
+namespace Export_RTF_File
+{
+    public static class SampleInterop
+    {
+        public static ValueTask<T> SaveAs<T>(this IJSRuntime JSRuntime, string filename, byte[] data)
+        {
+            try
+            {
+                return JSRuntime.InvokeAsync<T>("saveAsFile", filename, Convert.ToBase64String(data));
+            }
+            catch (Exception e)
+            {
+                return SampleInterop.LogError<T>(JSRuntime, e, "");
+            }
+        }
+
+        public static ValueTask<T> LogError<T>(IJSRuntime jsRuntime, Exception e, string message = "")
+        {
+
+            ErrorMessage error = new ErrorMessage();
+            error.Message = message + e.Message;
+            error.Stack = e.StackTrace;
+            if (e.InnerException != null)
+            {
+                error.Message = message + e.InnerException.Message;
+                error.Stack = e.InnerException.StackTrace;
+            }
+            return jsRuntime.InvokeAsync<T>(
+                "jsInterop.throwError", error);
+        }
+    }
+    public class ErrorMessage
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
+        [JsonProperty("stack")]
+        public string Stack { get; set; }
+    }
+}
